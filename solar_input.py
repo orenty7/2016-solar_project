390039b2221ef4682bd1fe127e06dd2a16839671
Artyom <mrartemail@gmail.com>
2021-11-09 15:06:56 +0300

    Fixed read_data function, fixed objects parsers; fixed write data to file function

--- solar_input.py
+++ solar_input.py
@@ -19,10 +19,14 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: do the same for planet
+            if object_type == "star":
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)
             else:
                 print("Unknown space object")
 
@@ -44,7 +48,7 @@ def parse_star_parameters(line, star):
     **star** — объект звезды.
     """
     _, r, color, m, x, y, Vx, Vy = line.split()
-    star.r = float(r)
+    star.R = int(r)
     star.color = color
     star.m = float(m)
     star.x = float(x)
@@ -69,7 +73,7 @@ def parse_planet_parameters(line, planet):
     **planet** — объект планеты.
     """
     _, r, color, m, x, y, Vx, Vy = line.split()
-    planet.r = float(r)
+    planet.R = int(r)
     planet.color = color
     planet.m = float(m)
     planet.x = float(x)
@@ -91,11 +95,16 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
-
-
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+            print("{type} {radius} {color} {mass} {x} {y} {velocity_x} {velocity_y}".format(
+                type = obj.type,
+                radius = obj.R,
+                color = obj.color,
+                mass = obj.m,
+                x = obj.x,
+                y = obj.y,
+                velocity_x = obj.Vx,
+                velocity_y = obj.Vy
+            ), file=out_file)
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
